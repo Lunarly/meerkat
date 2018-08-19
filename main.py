8d54d1678dd33c03462528b071677e4b93fc985c
Théo <kodle@users.noreply.github.com>
2018-08-19 21:42:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import
 from __future__ import print_function
 from tkinter import *
+from tkinter.filedialog import askdirectory
 import os, sys
 
 #
@@ -8,35 +9,40 @@ root = Tk()
 root.title("Meerkat")
 root.geometry("300x300")
 
-path = '.'
-
 # commande inutile
 def nope():
    filewin = Toplevel(root)
    button = Button(filewin, text="nope")
    button.pack()
 
-# scan
-var_label = StringVar()     #Zone de communication avec objet TkInter/Label
-label=Label(root,textvariable=var_label).pack() # Association Label et StringVar
+"""# selection du dossier WIP
+def selection():
+    folder = askdirectory()
+    path = str(folder) """
 
-files = os.listdir(path)
-for name in files:
-    print(name)
-    var_label.set(name)
+# scan
+def scan():
+    var_label = StringVar()     #Zone de communication avec objet TkInter/Label
+    label=Label(root,textvariable=var_label).pack() # Association Label et StringVar
+    files = os.listdir(path)
+    for name in files:
+        print(name)
+        var_label.set(name)
 
 # menu
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff = 0)
-filemenu.add_command(label="test", command = nope)
+filemenu.add_command(label="Selection du dossier", command = selection)
+filemenu.add_command(label="Scan", command = scan)
 
 filemenu.add_separator()
 
 filemenu.add_command(label = "Quitter", command = root.quit)
 menubar.add_cascade(label = "Options", menu = filemenu)
 editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label = "Undo", command = nope)
+
 
 
 root.config(menu = menubar)
+
 root.mainloop()
